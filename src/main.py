c9a38aefa49aac56d1b0f461a330dcebadabd3e1
Laura <rw21062006@gmail.com>
2025-04-29 18:17:06 +0200

    store api key in .env

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+from dotenv import load_dotenv, find_dotenv
+import os
+
+dotenv_path = find_dotenv()
+if dotenv_path == "":
+    with open('.env', 'w') as f:
+        token = input("Enter your last.fm API key: ")
+        f.write(f"TOKEN = {token}\n")
+load_dotenv(dotenv_path)
+
+token = os.getenv("TOKEN")
+print(f"Token is {token}")
